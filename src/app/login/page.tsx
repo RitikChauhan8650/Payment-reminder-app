e422d713578d60caf6c6744eb7fc9f3eca7ac23d
Ritik Chauhan <ritikchauhan8650@gmail.com>
2025-08-18 23:51:35 +0530

    login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -24,6 +24,7 @@ export default function LoginPage() {
             } else {
                 alert(data.message || "Login failed");
             }
+
         } catch (err) {
             console.error(err);
             alert("Error logging in");
